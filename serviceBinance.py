b5bd251029636ae2ac571b39e15f56b0262bab91
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 10:55:48 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -11,7 +11,7 @@ while True:
         ticker = functionsBinance.getklines(coin,tFrame,1)
         try:
                 functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-                print(ticker["Close"][0])
+                print(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
         except:
                 print("ERROR INSERTANDO DATOS")
         time.sleep(5)
