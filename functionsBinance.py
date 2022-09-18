09ecfe73397968450e65678eb3b5fab8c7b5530b
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 17:22:04 -0500

    Update functionsBinance.py

--- functionsBinance.py
+++ functionsBinance.py
@@ -1,10 +1,35 @@
 import os
 import pandas as pd
+from datetime import *
+from dateutil.relativedelta import *
 from binance.client import Client
 client = Client(os.environ['apiKeyBinance'], os.environ['apiSecBinance'], tld='com')
 
-def getklines(p_symbol,p_interval='1h',p_limit=1000):
-    frame = pd.DataFrame(client.get_klines(symbol=p_symbol, interval=p_interval, limit=p_limit),
+dStart = datetime.now()
+nEnd = dStart + relativedelta(years=-1)
+
+
+def getklineshistorial(p_symbol="BTCUSDT",p_interval='1m',nEnd=(dStart + relativedelta(years=-1)),dStart=datetime.now()):
+
+    while nEnd < dStart:
+        nDate = dStart + relativedelta(days=-1)
+        dateStart = date.strftime(dStart,"%d %b, %Y")
+        nextEnd = date.strftime(nDate,"%d %b, %Y")
+        dStart = dStart + relativedelta(days=-1)
+        print(nextEnd+" - "+dateStart)
+        frame = pd.DataFrame(client.get_klines(symbol=p_symbol, interval=p_interval, limit=p_limit,startTime=nextEnd,endTime=dateStart),
+                columns = ['Open_time','Open','High','Low','Close','Volume','Close_time',
+                'Quote_asset_volume','Number_of_trades','Taker_buy_base_asset_volume','Taker_buy_quote_asset_volume','Can_be_ignored'])
+        frame = frame.iloc[:,:11]
+        frame.Open_time = pd.to_datetime(frame.Open_time, unit='ms')
+        frame.Close_time = pd.to_datetime(frame.Close_time, unit='ms')
+        print(frame)
+    return frame
+
+
+
+def getklines(p_symbol,p_interval='1h',p_limit=1000,timeStart=,timeEnd):
+    frame = pd.DataFrame(client.get_klines(symbol=p_symbol, interval=p_interval, limit=p_limit,startTime=timeStart,endTime=timeEnd),
             columns = ['Open_time','Open','High','Low','Close','Volume','Close_time',
             'Quote_asset_volume','Number_of_trades','Taker_buy_base_asset_volume','Taker_buy_quote_asset_volume','Can_be_ignored'])
     frame = frame.iloc[:,:11]
