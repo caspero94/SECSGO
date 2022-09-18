ca30c1de5cb86f795c7782392c4e7f7e0c061726
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 17:42:48 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -3,10 +3,22 @@ import functionsDynamo
 import time
 coin = "BTCUSDT"
 tFrame = "1m"
+# CREATE TABLE IF NO EXITS
 try:
         functionsDynamo.create_table(coin)
 except:
         print("DB NAME REPEAT")
+# REVISAR HISTORIAL
+
+
+#####
+ticker = functionsBinance.getklines(coin,tFrame,1)
+        try:
+                functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
+                print(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
+        except:
+                print("ERROR INSERTANDO DATOS")
+        
 while True:
         ticker = functionsBinance.getklines(coin,tFrame,1)
         try:
