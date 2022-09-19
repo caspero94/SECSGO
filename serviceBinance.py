dba97552009af4660eee627a4306dbba9385e39f
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 20:25:23 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -12,7 +12,7 @@ except:
 print("REVISANDO HISTORIAL")
 ticker = functionsBinance.getklineshistorial(coin,tFrame)
 print(ticker)
-
+time.sleep(5)
 try:
         functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
         print("INSERTADO: "+coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
@@ -20,11 +20,11 @@ except:
         print("ERROR INSERTANDO DATOS")
 # ACTUALIZAR 2 ULTIMAS VELAS
 print("ACTUALIZAR ULTIMAS VELAS")
-while True:
-        ticker = functionsBinance.getklines(coin,tFrame,1)
-        try:
-                functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-                print("ACTUALIZANDO: "+coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-        except:
-                print("ERROR INSERTANDO DATOS")
-        time.sleep(5)
+#while True:
+ticker = functionsBinance.getklines(coin,tFrame,1)
+try:
+        functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
+        print("ACTUALIZANDO: "+coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
+except:
+        print("ERROR INSERTANDO DATOS")
+time.sleep(5)
