33ec125ceaba3aec413994422058e8b5df48b9ab
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 20:16:09 -0500

    Update functionsBinance.py

--- functionsBinance.py
+++ functionsBinance.py
@@ -14,7 +14,7 @@ def getklineshistorial(p_symbol="BTCUSDT",p_interval='5m'):
         nextEnd = date.strftime(nDate,"%d %b, %Y")
         dStart = dStart + relativedelta(days=-1)
         print(nextEnd+" - "+dateStart)
-        frame = pd.DataFrame(client.get_historical_klines(symbol=p_symbol, interval=p_interval,nextEnd,dateStart),
+        frame = pd.DataFrame(client.get_historical_klines(p_symbol, p_interval,nextEnd,dateStart),
                 columns = ['Open_time','Open','High','Low','Close','Volume','Close_time',
                 'Quote_asset_volume','Number_of_trades','Taker_buy_base_asset_volume','Taker_buy_quote_asset_volume','Can_be_ignored'])
         frame = frame.iloc[:,:11]
