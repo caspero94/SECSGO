29ad53d7eefb1eb8070183d240725b7a33e928f0
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 21:33:36 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -9,15 +9,9 @@ try:
 except:
         print("DB NAME REPEAT")
 # REVISAR HISTORIAL
-print("REVISANDO HISTORIAL")
 ticker = functionsBinance.getklineshistorial(coin,tFrame)
-print(ticker)
 time.sleep(30)
-try:
-        functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-        print("INSERTADO: "+coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-except:
-        print("ERROR INSERTANDO DATOS")
+
 # ACTUALIZAR 2 ULTIMAS VELAS
 print("ACTUALIZAR ULTIMAS VELAS")
 while True:
