f27aa7603f2851ab81030c956dd975a280ba76fb
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 20:17:22 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -12,6 +12,7 @@ except:
 
 ticker = functionsBinance.getklineshistorial(coin,tFrame)
 print(ticker)
+time.sleep(60)
 try:
         functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
         print(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
