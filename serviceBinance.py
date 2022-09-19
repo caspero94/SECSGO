7b2ea09fc1c7a8a66cdff22379d2b107af5ef217
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-18 20:02:18 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -12,11 +12,11 @@ except:
 
 ticker = functionsBinance.getklineshistorical(coin,tFrame)
 print(ticker)
-        try:
-                functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-                print(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
-        except:
-                print("ERROR INSERTANDO DATOS")
+try:
+        functionsDynamo.create_item(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
+        print(coin,tFrame,str(ticker["Open_time"][0]),ticker["Open"][0],ticker["High"][0],ticker["Low"][0],ticker["Close"][0],ticker["Volume"][0])
+except:
+        print("ERROR INSERTANDO DATOS")
 # ACTUALIZAR 2 ULTIMAS VELAS        
 while True:
         ticker = functionsBinance.getklines(coin,tFrame,1)
