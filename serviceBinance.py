e07599b3567d12572b7c807f91d88526ced1384b
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-21 00:59:17 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -2,7 +2,7 @@ import functionsBinance
 import functionsDynamo
 import time
 coin = "BTCUSDT"
-tFrame = "15m"
+tFrame = "1d"
 print("------------------------------------------------") 
 print("---- INICIANDO RECOLECTOR DE "+coin+" EN "+tFrame+" ----")
 print("------------------------------------------------") 
