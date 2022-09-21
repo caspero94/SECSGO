6f2f52a427872c572baff4d8691a2d4f67548f4b
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-21 01:28:19 -0500

    Update serviceBinance.py

--- serviceBinance.py
+++ serviceBinance.py
@@ -8,7 +8,7 @@ print("---- INICIANDO RECOLECTOR DE "+coin+" EN "+tFrame+" ----")
 print("------------------------------------------------") 
 # CREAR TABLA Y REVISAR HISTORIAL
 ticker = functionsBinance.getklineshistorial(coin,tFrame)
-time.sleep(30)
+time.sleep(5)
 
 # ACTUALIZAR 2 ULTIMAS VELAS
 print("------------------------------------------------") 
