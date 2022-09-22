3f53cdb6d66c33d8c2f1f9281ff57ce4d334ebc4
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-22 00:33:42 -0500

    Update functionsDynamo.py

--- functionsDynamo.py
+++ functionsDynamo.py
@@ -71,7 +71,8 @@ def get_tables():
     import pandas as pd
     dynamodb = boto3.resource('dynamodb')
     tables = list(dynamodb.tables.all())
+    
+    tables = pd.DataFrame (tables)
     tables = [table.replace('dynamodb.Table','') for table in tables]
-    #tables = pd.DataFrame (tables)
     print(tables)
     return tables
