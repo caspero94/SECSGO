1b2f917c4cfde81b6165710b921f02fdc44caa1d
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-23 19:29:13 -0500

    Update functionsDynamo.py

--- functionsDynamo.py
+++ functionsDynamo.py
@@ -75,7 +75,7 @@ def get_tables():
     for x in tables:
         x = str(x).replace("dynamodb.Table(name='","")
         x = str(x).replace("')","")
-        listo = lista.append(str(x))
+        listo = listo.append(str(x))
     print(listo)
     #tables = pd.DataFrame (tables)
     #tables = [table.replace('dynamodb.Table','') for table in tables]
