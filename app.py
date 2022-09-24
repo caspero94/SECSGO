76bc9060de9391c566259bcc61b2b372770b0782
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-23 20:14:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 activos = functionsDynamo.get_tables()
 filtro_activo = st.selectbox("ACTIVOS",options=activos)
 datachart = pd.DataFrame(functionsDynamo.get_chart())
-print(datachar)
+print(datachart)
 
 
 
