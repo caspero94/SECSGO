7134780c08829a6d5920aa36903c7c10d1b18158
caspero94 <84985267+caspero94@users.noreply.github.com>
2022-09-25 00:54:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ fin = st.date_input(
     "Hasta",
     datetime.date(2017, 8, 7))
 st.write(fin)
+print(fin)
 data_activo = pd.DataFrame(functionsDynamo.get_chart(filtro_activo))
 #data_activo = data_activo.drop(0)
 
